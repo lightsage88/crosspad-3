8f3832ab26f3131990a7cdddc5c493a3dfabc84c
lightsage88 <adrian.e.rosales@gmail.com>
2022-04-20 01:37:45 -0700

    I got the searching of games to work just how I want it.

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -7,6 +7,7 @@ import GiantBombGameSearchStorage from '../../data/giant-bomb-game-search-storag
 function Search(props) {
     const [open, setOpen] = useState(false);
     const [options, setOptions] = useState([]);
+    const [queryString, setQueryString] = useState('');
     const loading = open && options.length === 0;
 
     function sleep(delay = 0) {
@@ -15,33 +16,31 @@ function Search(props) {
         });
     }
 
+    function handleSearchInputChange(e) {
+        console.log('handleSearchInputChange running...', e.target.value);
+        setOptions([]);
+        setQueryString(e.target.value);
+    }
+
     useEffect(() => {
+        console.log('useEffect running...');
         let active = true;
         if (!loading) {
             return undefined;
         }
 
         (async () => {
-            await sleep(2);
+            await sleep(1500);
 
             if (active) {
+                console.log('active is true');
                 //TODO: Make request to server to get bunch of GiantBomb data
                 let searchResponse = await GiantBombGameSearchStorage.read({
                     resourceName: 'search',
                     searchResource: 'game',
-                    queryString: 'Dark Souls'
+                    queryString
                 });
                 console.log('PULANI OF GUAM', searchResponse);
-                // setOptions([
-                //     {
-                //         title: 'Super Smash Bros. Ultimate',
-                //         year: 2018
-                //     },
-                //     {
-                //         title: 'Metal Gear Solid 2: Sons of Wolbocho',
-                //         year: 2001
-                //     }
-                // ]);
                 searchResponse.data = searchResponse.data.map((el, index) => {
                     el.key = index;
                     return el;
@@ -54,7 +53,7 @@ function Search(props) {
         return () => {
             active = false;
         };
-    }, [loading]);
+    }, [queryString]);
 
     return (
         <div id="search-div">
@@ -71,41 +70,25 @@ function Search(props) {
                 }}
                 isOptionEqualToValue={(option, value) => option.Name === value.Name}
                 getOptionLabel={(option) => option?.Name}
-                renderOption={(props, option) => {
-                    if (open) {
-                        return (
-                            <div key={option.key}>
-                                <img src={option.Icon_Image_URL} />
-                                {option.Name}
-                            </div>
-                        );
-                    } else {
-                        return (
-                            option.Name
-                        );
-                    }
-                }
-                }
                 options={options}
                 loading={loading}
-                renderInput={(params) => {
-                    console.log('hokookok', params);
-                    return (
-                        <TextField
-                            {...params}
-                            label="Seek a Game"
-                            InputProps={{
-                                ...params.InputProps,
-                                endAdornment: (
-                                    <>
-                                        {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                                        {params.InputProps.endAdornment}
-                                    </>
-                                ),
-                            }}
-                        />
-                    );
-                }}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Seek a Game"
+                        InputProps={{
+                            ...params.InputProps,
+                            endAdornment: (
+                                <>
+                                    {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                    {params.InputProps.endAdornment}
+                                </>
+                            ),
+                        }}
+                        onChange={(e) => handleSearchInputChange(e)}
+                    />
+                )
+                }
             />
         </div>
     );
